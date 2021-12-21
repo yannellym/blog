02ec215fcfe89989f25a2762fd81dc233adc20fa
yannelly mercado <yannellym@gmail.com>
2021-12-20 19:07:34 -0500

    second commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require('lodash');
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -50,6 +51,24 @@ const newPost = {
 posts.push(newPost);
 res.redirect("/");
 });
+
+app.get("/posts/:postName", function(req,res){
+  let newRequest = _.lowerCase(req.params.postName);
+
+posts.forEach(function(post) {
+let allTitles = _.lowerCase(post.title);
+
+  if (allTitles === _.lowerCase([string=newRequest])) {
+    console.log("Match found!");
+}  else{
+  console.log("Not a match");
+}
+});
+});
+
+
+
+
 
 
 
