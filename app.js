7e6cf83a3df6def66910448d061b3bf5c0294ffc
yannelly mercado <yannellym@gmail.com>
2021-12-21 19:04:10 -0500

    last commit

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const ejs = require("ejs");
 var _ = require('lodash');
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Welcome!";
+const aboutContent = "About Me";
+const contactContent = "Email: yannellym@gmail.com";
 let posts = [];
 
 const app = express();
@@ -25,7 +25,9 @@ app.get("/", function(req,res) {
   posts: posts });
 });
 
-
+app.get("/myposts", function(req,res) {
+  res.render("myposts", {posts: posts })
+})
 
 app.get("/about", function(req,res){
   res.render("about", {aboutContent: aboutContent} );
@@ -45,28 +47,31 @@ app.get("/compose", function(req,res) {
 app.post("/compose", function(req,res) {
 const newPost = {
   title: req.body.newComposeTitle, 
-  post: req.body.newComposeBody
+  body: req.body.newComposeBody
 };
   
 posts.push(newPost);
-res.redirect("/");
+res.redirect("/myposts");
 });
 
 app.get("/posts/:postName", function(req,res){
+  console.log("REQ: ", req.params)
   let newRequest = _.lowerCase(req.params.postName);
 
-posts.forEach(function(post) {
-let allTitles = _.lowerCase(post.title);
+  posts.forEach(function(post) {
+  let allTitles = _.lowerCase(post.title);
 
-  if (allTitles === _.lowerCase([string=newRequest])) {
-    res.render("post", {
-      title: post.title,
-      content: post.content
-    });
-} 
+    if (allTitles === newRequest) {
+      res.render("post", {
+        title: post.title,
+        body: post.body
+      });
+    } 
 
+  });
 });
-});
+
+
 
 
 
